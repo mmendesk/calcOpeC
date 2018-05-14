fe19f1143fb247537c0ab0893ce8cdcd7a5675d0
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2018-05-13 21:20:31 -0300

    base u2

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,6 @@
 function calculadora() {
     if (document.getElementById("inTresConj").checked) {
         alert("TRES");
-
-
-
-    } else if (document.getElementById("inDoisConj").checked) {
-        alert("DOIS");
         var Res = 0;
         var U = document.getElementById("inUniverso").value;
         var A = document.getElementById("inA").value;
@@ -16,15 +11,28 @@ function calculadora() {
         var BmC = document.getElementById("inBmC").value;
         var AmBmC = document.getElementById("inAmBmC").value;
 
+    } else if (document.getElementById("inDoisConj").checked) {
+        alert("DOIS");
+        var Res = 0;
+        var U = document.getElementById("inUniverso").value;
+        var A = document.getElementById("inA").value;
+        var B = document.getElementById("inB").value;
+        var AmB = document.getElementById("inAmB").value;
+
+
         if (U == "x") {
-            alert("-U");
+            a = Number(A);
+            b = Number(B);
+            amb = Number(AmB);
+            Res = a + b + amb;
+            var current = document.getElementById("inUniverso");
+            current.value = Res
         }
         else if (A == "x") {
             u = Number(U);
             b = Number(B);
             amb = Number(AmB);
             Res = u - (b + amb);
-
             var current = document.getElementById("inA");
             current.value = Res
         }
@@ -32,7 +40,6 @@ function calculadora() {
             u = Number(U);
             a = Number(A);
             b = Number(B);
-
             Res = u - (a + b);
             var current = document.getElementById("inAmB");
             current.value = Res
@@ -54,4 +61,26 @@ var verificador = document.getElementById("btVer");
 verificador.addEventListener("click", calculadora);
 
 
+function doalertA(checkboxElem) {
+    if (checkboxElem.checked) {
+        op3.style.display = 'none';
+        op4.style.display = 'none';
+        op5.style.display = 'none';
+        op6.style.display = 'none';
+    } else {
+      alert ("bye");
+    }
+  }
+  function doalertB(checkboxElem) {
+    if (checkboxElem.checked) {
 
+      op3.style.display = 'block';
+      op4.style.display = 'block';
+      op5.style.display = 'block';
+      op6.style.display = 'block';
+
+
+    } else {
+      alert ("bye");
+    }
+  }
